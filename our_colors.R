dae7f8c8a8c22d5ae27d863cc8e958f952444489
signeromer <111518714+signeromer@users.noreply.github.com>
2025-10-14 15:25:05 +0200

    add company_colors.R

---
+++ our_colors.R
@@ -0,0 +1,19 @@
+# ===============================================
+# COMPANY COLORS — enkel version
+# ===============================================
+
+company_colors <- c(
+  red   = "#c6302c",
+  blue  = "#004aad",
+  green = "#0d6256",
+  black = "#343434",
+  white = "#ffffff"
+)
+
+company_col <- function(...) {
+  cols <- c(...)
+  if (length(cols) == 0) return(company_colors)
+  company_colors[cols]
+}
+
+
